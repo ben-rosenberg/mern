71c467d74094795d1b71a39a9950ae9ea37637b7
Ben Rosenberg <brosenberg570@gmail.com>
2022-05-03 12:34:52 -0600

    Add the Ninja assignment

---
+++ Javascript/OOP/Ninja/Ninja.js
@@ -0,0 +1,37 @@
+class Ninja {
+    constructor(name, health, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName() {
+        console.log(`Name: ${this.name}`);
+    }
+
+    showStats() {
+        this.printDivider(` PRINTING ${(this.name.toUpperCase())}'S INFO `);
+        this.sayName();
+        console.log(`Strength: ${this.strength}`);
+        console.log(`Speed: ${this.speed}`);
+        console.log(`Health: ${this.health}`);
+    }
+
+    drinkSake() {
+        this.printDivider(" DRINKING SAKE ");
+        this.health += 10;
+        console.log(`Health increased by 10, new health: ${this.health}`);
+    }
+
+    printDivider(message) {
+        if (message === undefined) { message = ""; }
+        console.log(`**************${message}**************`);
+    }
+}
+
+
+const ben = new Ninja("Ben", 100);
+ben.showStats();
+
+ben.drinkSake();
---
+++ Javascript/OOP/Ninja/README.md
@@ -0,0 +1,3 @@
+# OOP Ninja
+
+The "introduction to OOP in JavaScript" assignment. A simple "Ninja" class with attributes and a few methods to print the instance's information.
