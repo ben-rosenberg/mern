ee3a946b16188cd1caf97f3c91712d3d2d803a3a
Ben Rosenberg <brosenberg570@gmail.com>
2022-05-10 14:37:11 -0600

    Remove ApiLearning directory

--- React/APIs/ApiLearning/script.js
+++
@@ -1,8 +0,0 @@
-fetch("https://pokeapi.co/api/v2/pokemon")
-    .then((respose) => {
-        return response.json;
-    }).then((response) => {
-        console.log(response);
-    }).catch((error) => {
-        console.log(error);
-    });
